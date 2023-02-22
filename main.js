245ce043ae30a7e72508b013ed91947b9261a809
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2023-02-23 08:56:02 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 400,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+    })
+    ipcMain.handle('directory', () => __dirname)
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
